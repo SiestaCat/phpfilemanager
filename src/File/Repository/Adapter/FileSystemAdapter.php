89204d93f0ac2ea4d624ce1574b5a5e385375277
SiestaCat <kratos@example.com>
2023-06-11 12:00:03 +0200

    Edit fs adapter

--- src/File/Repository/Adapter/FileSystemAdapter.php
+++ src/File/Repository/Adapter/FileSystemAdapter.php
@@ -8,6 +8,9 @@ use Siestacat\Phpfilemanager\File\Repository\AdapterInterface;
 final class FileSystemAdapter implements AdapterInterface
 {
 
+    const HASH_DIR_DEEP = 2;
+    const HASH_DIR_LENGTH = 2;
+
     private string $abs_path;
 
     public function __construct(string $abs_path)
@@ -40,16 +43,45 @@ final class FileSystemAdapter implements AdapterInterface
         return new File($hash, $path);
     }
 
+    public function exists(string $hash):bool
+    {
+        $path = $this->getPath($hash);
+
+        return is_file($path);
+    }
+
     public function del(string $hash):bool
     {
         $path = $this->getPath($hash);
 
-        return unlink($path);
+        $unlink = unlink($path);
+
+        $dir = dirname($path);
+
+        for($a=1;$a<=self::HASH_DIR_DEEP;$a++)
+        {
+
+            $this->checkEmptyDir($dir);
+
+            $dir = dirname($dir);
+        }
+
+        return $unlink;
     }
 
     public function getPath(string $hash):string
     {
         return $this->abs_path . $this->hashToRelPath($hash);
+    }
+
+    private function checkEmptyDir(string $dir):void
+    {
+
+        $files = scandir($dir);
+
+        if(!is_array($files)) return;
+
+        if(count($files) === 2) rmdir($dir);
     }
 
     private function hashToRelPath(string $hash):string
@@ -61,7 +93,7 @@ final class FileSystemAdapter implements AdapterInterface
             (
                 str_split
                 (
-                    substr($hash, 0, 4),
+                    substr($hash, 0, (self::HASH_DIR_DEEP * self::HASH_DIR_LENGTH)),
                     2
                 ),
                 [
